534a018305dba7ef69da58cfb80f65ed70092d08
erdincDonmez <erdinc_donmez@yahoo.com>
2022-11-07 00:29:09 +0300

    CLI Components

---
+++ Icerik/Bolum_0201_VueJs-CLI/src/components/MainContainer.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse a impedit illum similique cum facilis! Neque nostrum corporis velit iste quasi! Ut incidunt debitis vitae voluptatibus quo quae obcaecati aliquam?</p>
+        <p>{{text}}</p>
+    </div>
+
+
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima sequi placeat neque tempora pariatur. Ipsam amet maxime autem dignissimos fugit debitis neque, consequuntur sunt inventore, deserunt ex reprehenderit tenetur facere!"
+
+        }
+    },
+    methots : {
+
+    },
+    conputed : {
+
+    },
+    watch : {
+        
+    }
+}
+</script>
+
+<style>
+</style>
